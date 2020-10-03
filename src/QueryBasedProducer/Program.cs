d89a83483a10b400258b32dfc1368d066f1a865a
Jamie Bowman <mrjamiebowman@gmail.com>
2020-10-03 19:25:01 -0400

    QueryBasedProducer

---
+++ src/QueryBasedProducer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueryBasedProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
